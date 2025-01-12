e24e9ad35af19f191bac320c7a0780222735ac56
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-01-12 02:09:22 +0000

    navbar added

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,13 +1,11 @@
 
-
+import Navbar from "./components/Navbar";
 
 function App() {
 
   return (
     <div>
-     <h1 className="text-xl font-bold underline ">
-      Hello world!
-    </h1>
+     <Navbar />
     </div>
   );
 }

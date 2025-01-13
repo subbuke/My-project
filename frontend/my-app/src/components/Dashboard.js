6a9e4cbf35df43995374be84dee96c6dffa5f49c
subramanyam <141896120+subbuke@users.noreply.github.com>
2025-01-13 12:26:05 +0000

    table added

--- frontend/my-app/src/components/Dashboard.js
+++ frontend/my-app/src/components/Dashboard.js
@@ -9,10 +9,138 @@ const Dashboard = () => {
             <p className="m-5 ">In 2014, it ranked sixth in average attendance among all sports leagues.[6] In 2010, the IPL became the first sporting event to be broadcast live on YouTube.[7][8] Inspired by the success of the IPL, other Indian sports leagues have been established.[a][11][12][13] In 2022, the league's brand value was estimated at ₹90,038 crore (US$11 billion).[14] According to the BCCI, the 2015 IPL season contributed ₹1,150 crore (US$140 million) to India's GDP.[15] In December 2022, the IPL achieved a valuation of US$10.9 billion, becoming a decacorn and registering a 75% growth in dollar terms since 2020 when it was valued at $6.2 billion, according to a report by the consulting firm D and P Advisory.[16] Its 2023 final became the most streamed live event on the internet, with 32 million viewers.[17]
 
                 In 2023, the league sold its media rights for the next 4 seasons for US$6.4 billion to Viacom18 and Star Sports,[18] meaning each IPL match was valued at $13.4 million.[19] As of 2024, there have been seventeen seasons of the tournament. The current champions are the Kolkata Knight Riders, who won the 2024 season after defeating the Sunrisers Hyderabad in the final.[20]</p>
-             <h1 className="text-center font-black text-2xl">History</h1>
-             <p className="m-5">
-             In 2007, Zee Entertainment Enterprises founded the Indian Cricket League (ICL).[21] The ICL was not recognized by the Board of Control for Cricket in India (BCCI) or the International Cricket Council (ICC). Moreover, the BCCI was unhappy about its committee members joining the ICL executive board.[22] In response, the BCCI increased the prize money for its domestic tournaments and imposed lifetime bans on players who joined the rival league, which it considered a rebel league.[23][24]
-             </p>
+            <h1 className="text-center font-black text-2xl">History</h1>
+            <p className="m-5">
+                In 2007, Zee Entertainment Enterprises founded the Indian Cricket League (ICL).[21] The ICL was not recognized by the Board of Control for Cricket in India (BCCI) or the International Cricket Council (ICC). Moreover, the BCCI was unhappy about its committee members joining the ICL executive board.[22] In response, the BCCI increased the prize money for its domestic tournaments and imposed lifetime bans on players who joined the rival league, which it considered a rebel league.[23][24]
+            </p>
+            <h1 className="text-center font-black text-2xl">Foundation</h1>
+            <p className="m-5"> On 13 September 2007,[25] following India's victory at the 2007 T20 World Cup,[26] the BCCI announced a franchise based Twenty20 cricket competition known as the Indian Premier League. The inaugural season was scheduled to start in April 2008, commencing with a "high-profile ceremony" in New Delhi. BCCI Vice-president Lalit Modi, who led the IPL initiative, provided details of the tournament, including its format, prize money, franchise revenue system, and squad composition rules. The league, to be managed by a seven-man governing council, would also serve as the qualifying mechanism for that year's Champions League Twenty20.[25]
+
+                To determine team ownership, an auction for the franchises was held on 24 January 2008. The reserve prices for the eight franchises totalled $400 million,[24] but the auction ultimately raised $723.59 million.[27] The league officially commenced in April 2008, featuring Chennai Super Kings (CSK), Mumbai Indians (MI), Delhi Daredevils (DD), Kings XI Punjab (KXIP), Deccan Chargers (DC), Rajasthan Royals (RR), Kolkata Knight Riders (KKR), and Royal Challengers Bangalore (RCB).[28]
+
+                Following the ban on players who chose to participate in the ICL, the rival league shut down in 2009.[29][30]</p>
+            <h1 className="text-center font-black text-2xl">Expansions and terminations</h1>
+            <p>New franchises, Pune Warriors India and Kochi Tuskers Kerala, joined the league before the fourth season in 2011.[31] The Sahara Adventure Sports Group purchased the Pune franchise for $370 million, while Rendezvous Sports World bought the Kochi franchise for $333.3 million.[31] The Kochi franchise was terminated after just one season due to their failure to pay the BCCI the 10% bank guarantee element of the franchise fee.[32]
+
+                In September 2012, the Deccan Chargers franchise agreement was terminated after the BCCI failed to find new owners.[33] In October, an auction was held for a replacement franchise; Sun TV Network won the bid for what became the Hyderabad franchise;[34] the team was named Sunrisers Hyderabad.[35]
+
+                Pune Warriors India withdrew from the IPL in May 2013 due to financial differences with the BCCI.[36] The BCCI officially terminated the franchise in October, and the league reverted to eight teams.[37]
+
+                In June 2015, the two-time champions Chennai Super Kings and the inaugural season champions Rajasthan Royals were suspended for two seasons following their involvement in a spot-fixing and betting scandal.[38] The two teams were replaced for two seasons by franchises based in Pune and Rajkot.[39][40]
+
+                Due to the COVID-19 pandemic, the venue for the 2020 season was moved and games were played in the United Arab Emirates.[41][42] In August 2021, the BCCI announced that two new franchises, based in two of six shortlisted cities, would join the league in the 2022 season.[43][44] In closed bidding held in October, the RPSG Group and CVC Capital won the bids for the teams, paying ₹7,000 crore (US$820 million) and ₹5,200 crore (US$610 million), respectively.[45][46] The teams were subsequently named Lucknow Super Giants and Gujarat Titans.
+
+                Several IPL franchise owners have expanded their business by acquiring teams in other franchise leagues, such as the Caribbean Premier League (CPL), South Africa's SA20, the UAE's International League T20 (ILT) and the USA's Major League Cricket (MLC). These teams have been branded with similar names to their parent IPL franchises.[47]</p>
+
+                <div className="relative overflow-x-auto m-5">
+  <table className="w-full text-sm text-left rtl:text-right text-gray-200 dark:text-gray-400">
+    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th scope="col" className="px-6 py-3">
+          Indian Premier League
+        </th>
+        <th scope="col" className="px-6 py-3">
+         CPL
+        </th>
+        <th scope="col" className="px-6 py-3">
+          SA20
+        </th>
+        <th scope="col" className="px-6 py-3">
+          ILT
+        </th>
+        <th scope="col" className="px-6 py-3">
+          MLC
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <th
+          scope="row"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+          Chennai super Kings
+        </th>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4">	Joburg Super Kings</td>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4"></td>
+      </tr>
+      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <th
+          scope="row"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+          Delhi Capitals
+        </th>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4">Pretoria Capitals</td>
+        <td className="px-6 py-4">Dubai Capitals</td>
+        <td className="px-6 py-4"></td>
+      </tr>
+      <tr className="bg-white dark:bg-gray-800">
+        <th
+          scope="row"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+         Gujarat Titans
+        </th>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4"></td>
+      </tr>
+      <tr className="bg-white dark:bg-gray-800">
+        <th
+          scope="row"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+         Kolkata Knight Riders
+        </th>
+        <td className="px-6 py-4">Trinbago Knight Riders	</td>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4">Abu Dhabi Knight Riders</td>
+        <td className="px-6 py-4">Los Angeles Knight Riders</td>
+      </tr>
+      <tr className="bg-white dark:bg-gray-800">
+        <th
+          scope="row"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+         Lucknow Super Giants
+        </th>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4">	Durban's Super Giant</td>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4"></td>
+      </tr>
+      <tr className="bg-white dark:bg-gray-800">
+        <th
+          scope="row"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+         Mumbai Indians
+        </th>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4">	MI Cape Town</td>
+        <td className="px-6 py-4">MI Emirates</td>
+        <td className="px-6 py-4">MI New York</td>
+      </tr>
+      <tr className="bg-white dark:bg-gray-800">
+        <th
+          scope="row"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+        >
+         Punjab Kings
+        </th>
+        <td className="px-6 py-4">Saint Lucia Kings</td>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4"></td>
+        <td className="px-6 py-4"></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
         </div>
     );
